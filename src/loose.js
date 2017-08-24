362fc7688f1ef09d6d9379a3ae3cfe6fc182d86e
Trev <teeerevor@gmail.com>
2017-08-24 23:23:58 +1000

    remove junk and tidy up

--- src/loose.js
+++ src/loose.js
@@ -1,20 +1,12 @@
 //resize arrow scale up/down
+//delete and remake arrows rather then toggle vis.
 
 const createArrowhead = (layer, headPosition, headType, headScale) => {
-  log(layer.class());
   const baseArrowSize = 4
   headPosition = (typeof headPosition !== 'undefined') ?  headPosition : 'end';
-  // arrowPosition - which end of path to attach arrowhead
-  // 0=begin, 1=end of path, 2=both ends
-
   headType = (typeof headType !== 'undefined') ?  headType : 'fill';
-  // arrowType - what shape arrowhead should be
-  // 0=filled triangle
-
   headScale = (typeof headScale !== 'undefined') ?  headScale : 'default';
-  // TBD pass in scale, allow for different ends for arrowhead
 
-  //split line depending on how curvy it is, with at least 2 points
   var path = layer.bezierPathWithTransforms();
   const lineThickness = layer.style().borders().firstObject().thickness();
   const length = path.length();
@@ -28,8 +20,6 @@ const createArrowhead = (layer, headPosition, headType, headScale) => {
   const linePoint = path.pointOnPathAtLength(arrowBase);
   const angle = 360/(2*Math.PI) * (Math.atan2(linePoint.y - endPoint.y, linePoint.x - endPoint.x));
 
-  //0 - triangle as arrowhead
-  //TBD: different shapes
   var headPath = NSBezierPath.bezierPath();
   headPath.moveToPoint(NSMakePoint(0,baseArrowSize));
   headPath.lineToPoint(NSMakePoint(-baseArrowSize*2,0));
@@ -40,14 +30,11 @@ const createArrowhead = (layer, headPosition, headType, headScale) => {
   headShape.frame().setWidth(Math.floor(headShape.frame().width()*scale));
   headShape.frame().setHeight(Math.floor(headShape.frame().height()*scale));
 
-  //rotate
   headShape.setRotation(-1*angle);
 
-  //move center to endpoint of line
   headShape.frame().setX(endPoint.x - headShape.frame().width()/2);
   headShape.frame().setY(endPoint.y - headShape.frame().height()/2);
 
-  //color same as line
   var fill = headShape.style().addStylePartOfType(0);
   fill.color = layer.style().borders().firstObject().color();
 
@@ -62,13 +49,11 @@ const createArrow = (doc, layer, arrowPosition, arrowType, arrowScale) => {
   arrowheadEnd.setName('ArrowheadEnd');
 
   //add to layer and group it with path
-
   var parent = layer.parentGroup();
   var group = MSLayerGroup.new();
   parent.removeLayer(layer);
   parent.addLayers([group]);
   group.addLayers([layer, arrowheadStart, arrowheadEnd]);
-  //group.adjustToFit();
   group.resizeToFitChildrenWithOption(0);
   if ((layer.name() == 'Line') || (layer.name() == 'Path')) {
     group.setName('Arrow');
@@ -80,29 +65,28 @@ const createArrow = (doc, layer, arrowPosition, arrowType, arrowScale) => {
 
 const toggleArrows = (group) => {
   var start, end;
-  var children = group.children();
-  for (var j = 0; j < children.count(); j++) {
-    var child = children[j];
+  const children = group.children();
+  children.forEach((child) => {
     if (child.name() == 'ArrowheadStart')
       start = child;
     if (child.name() == 'ArrowheadEnd')
       end = child;
+  });
+
+  var startOpacity = start.style().contextSettings().opacity();
+  var endOpacity = end.style().contextSettings().opacity();
+  if(startOpacity == 1 && endOpacity == 1 ){
+    start.style().contextSettings().setOpacity(0);
   }
-  if (end && start) {
-    var startOpacity = start.style().contextSettings().opacity();
-    var endOpacity = end.style().contextSettings().opacity();
-    if(startOpacity == 1 && endOpacity == 1 ){
-      start.style().contextSettings().setOpacity(0);
-    }
-    if(startOpacity == 0 && endOpacity == 1 ){
-      end.style().contextSettings().setOpacity(0);
-      start.style().contextSettings().setOpacity(1);
-    }
-    if(startOpacity == 1 && endOpacity == 0 ){
-      end.style().contextSettings().setOpacity(1);
-    }
+  if(startOpacity == 0 && endOpacity == 1 ){
+    end.style().contextSettings().setOpacity(0);
+    start.style().contextSettings().setOpacity(1);
+  }
+  if(startOpacity == 1 && endOpacity == 0 ){
+    end.style().contextSettings().setOpacity(1);
   }
 }
+
 const isCompletShape = (layer) =>{
   const path = layer.bezierPathWithTransforms();
   const start = path.pointOnPathAtLength(0);
@@ -141,12 +125,10 @@ const onRun = (context) => {
         arrows.push(layer);
       }
     }
-    //add arrows to selection so you can keep hitting
-    // cmd l to toggle arrowheads
-    for (var i = 0; i < arrows.length; i++) {
-      var arrow = arrows[i];
+    //push arrows to selection so you can keep toggeling + resize
+    arrows.forEach((arrow) => {
       document.layerWithID(arrow.objectID()).addToSelection();
-    }
+    });
   }
 }
 onRun(context);
